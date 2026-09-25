2776fbb200cbfe200f4489db58433b17e5af86e5
tariksdeg <tariksdegirmencii@gmail.com>
2026-09-25 15:30:07 +0300

    Yayin guncellendi

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SURUM = "ezber-138e822aa97e";
+const SURUM = "ezber-4e1c3ad81aba";
 const DOSYALAR = ["./", "./index.html", "./kelimeler.json",
                   "./manifest.webmanifest", "./ikon-192.png", "./ikon-512.png"];
 
